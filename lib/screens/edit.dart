5cad2055019fb56358f3c53a0f7aacb050e965db
ArshesSH <53480645+ArshesSH@users.noreply.github.com>
2022-04-07 19:02:08 +0900

    Add TextField

--- lib/screens/edit.dart
+++ lib/screens/edit.dart
@@ -5,6 +5,45 @@ class EditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: const [
+            TextField(
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: InputDecoration(
+                //border: OutlineInputBorder(),
+                hintText: 'Title',
+              ),
+            ),
+            
+            Padding(padding: EdgeInsets.all(10),),
+            
+            TextField(
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                decoration: InputDecoration(
+                  //border: OutlineInputBorder(),
+                  hintText: 'Contents',
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Text('save'),
+        icon: const Icon(Icons.save),
+      ),
+    );
   }
 }
